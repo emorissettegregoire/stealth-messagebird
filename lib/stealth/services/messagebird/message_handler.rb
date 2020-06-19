b887ee456fb04b82e365fb9d216930464f40c5de
Emilie Morissette <emorissettegregoire@gmail.com>
2020-06-19 10:34:38 +0400

    Changed params for sender_id

--- lib/stealth/services/messagebird/message_handler.rb
+++ lib/stealth/services/messagebird/message_handler.rb
@@ -26,11 +26,11 @@ module Stealth
         def process
           @service_message = ServiceMessage.new(service: 'messagebird')
           # service_message.sender_id = params['From']
-          service_message.sender_id = params["contact"]["msisdn"]
+          # service_message.sender_id = params["contact"]["msisdn"]
+          service_message.sender_id = params["message"]["from"]
 
           # service_message.message = params['Body']
           service_message.message = params["message"]["content"]["text"]
-
           # Check for media attachments
 
           #### Need to be changed for messagebird params
