3113b11905b77e1ccd7aeaea669b4e929b0217ad
Emilie <45463745+emorissettegregoire@users.noreply.github.com>
2021-04-19 11:22:24 +0400

    Update lib/stealth/services/messagebird/messagebird_service_message.rb
    Co-authored-by: Sunny Ripert <sunny@sunfox.org>

--- lib/stealth/services/messagebird/messagebird_service_message.rb
+++ lib/stealth/services/messagebird/messagebird_service_message.rb
@@ -2,7 +2,12 @@ module Stealth
   module Services
     module Messagebird
       class MessagebirdServiceMessage < Stealth::ServiceMessage
-        attr_accessor :conversation_id, :messagebird_id, :platform, :display_name, :first_name, :last_name
+        attr_accessor :conversation_id,
+                      :messagebird_id,
+                      :platform,
+                      :display_name,
+                      :first_name,
+                      :last_name
 
       end
     end
